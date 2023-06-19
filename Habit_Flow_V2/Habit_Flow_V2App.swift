e15ee57aeec467901de0c06a1227e7f8f9b7d894
Lena Ngo <thuy.ngo@student.hs-rm.de>
2023-06-19 19:55:52 +0200

    Initial Commit

---
+++ Habit_Flow_V2/Habit_Flow_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_Flow_V2App.swift
+//  Habit_Flow_V2
+//
+//  Created by Lena Ngo on 19.06.23.
+//
+
+import SwiftUI
+
+@main
+struct Habit_Flow_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
