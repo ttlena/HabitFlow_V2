58403660634abd3b752021d04a84f5768c9604ef
Marc Bachmann <marc.bachmann99@googlemail.com>
2023-08-04 19:48:37 +0200

    added intro screens (not ready)

--- Habit_Flow_V2/Intro/PageModel.swift
+++ Habit_Flow_V2/Intro/PageModel.swift
@@ -17,7 +17,8 @@ struct Page: Identifiable, Equatable {
     static var samplePage = Page(name: "Title Example", description: "Sample Description for Example", imageUrl: "Success", tag: 0)
     
     static var samplePages: [Page] = [
-    Page(name: "Wilkommen bei Habit Flow 1", description: "Description Test", imageUrl: "Success", tag: 0),
-    Page(name: "Wilkommen bei Habit Flow 2", description: "Description Test", imageUrl: "Success", tag: 1),
-    Page(name: "Wilkommen bei Habit Flow 3", description: "Description Test", imageUrl: "Success", tag: 2)]
+    Page(name: "Wilkommen bei Habit Flow", description: "Description Test", imageUrl: "Success", tag: 0),
+    Page(name: "Alles unter einem Hut", description: "Habit Flow fasst einen Kalender, deine ToDo Liste sowie deine Habits in einer App zusammen.", imageUrl: "Success", tag: 1),
+    Page(name: "Benachrichtigungen", description: "Lasse Benachrichtigungen zu, damit du keine Termine verpasst und deine ToDo's und Habits jeden Tag erledigst", imageUrl: "Notification", tag: 2),
+    Page(name: "Los geht's!", description: "Du bist nun startklar.", imageUrl: "Done", tag: 3)]
 }
